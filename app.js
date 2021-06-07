5f5f041bab661a96c07bbed89ce4152158da9d4a
vivekgudipudi <37794298+vivekgudipudi@users.noreply.github.com>
2021-06-07 13:27:39 +0530

    update: added CSS

--- app.js
+++ app.js
@@ -5,25 +5,24 @@ var outputDiv = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json";
 
-function getTranslationURL(text){
+function getTranslationURL(text) {
     return serverURL + "?" + "text=" + text;
 
 }
 
 
-function errorHandler(error){
-    console.log("error occured",error);
+function errorHandler(error) {
+    console.log("error occured", error);
     alert("Something is wrong with server, please try again later")
 }
 
-function clickEvent(){
+function clickEvent() {
     fetch(getTranslationURL(txtInput.value))
-    .then(response => response.json())
-    .then(json => outputDiv.innerText=json.contents.translated )
-    
+        .then(response => response.json())
+        .then(json => outputDiv.innerText = json.contents.translated)
 
-    .catch(errorHandler);
+
+        .catch(errorHandler);
 };
 
-btnTranslate.addEventListener("click",clickEvent)
-
+btnTranslate.addEventListener("click", clickEvent)
